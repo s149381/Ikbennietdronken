3e8a1ac2a04c58942eb5fab2b15291eb4546f81d
s149381 <jordi-klarenbeek@live.nl>
2016-01-13 16:11:27 +0100

    First test with full passport adds

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AA Website' });
-});
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/login');
+}
 
-router.get('/login', function(req,res,next) {
-  res.render('login', { title: 'Login user'})
-});
+module.exports = function(passport){
+	
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+	  res.render('index', { title: 'AA Website' });
+	});
+	
+	/* Get login page */
+	router.get('/login', function(req,res,next) {
+	  res.render('login', { message: req.flash('message') 
+	});
+	
+	/* Handle Login POST */
+	router.post('/login', passport.authenticate('login', {
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash : true  
+	}));
+	
+	/* GET Registration Page */
+	router.get('/signup', function(req, res){
+		res.render('register',{message: req.flash('message')});
+	});
 
-module.exports = router;
+	/* Handle Registration POST */
+	router.post('/signup', passport.authenticate('signup', {
+		successRedirect: '/profile',
+		failureRedirect: '/signup',
+		failureFlash : true  
+	}));
+	
+	/* GET Home Page */
+	router.get('/profile', isAuthenticated, function(req, res){
+		res.render('home', { user: req.user });
+	});
+
+	/* Handle Logout */
+	router.get('/signout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+	
+	return router;
+}
