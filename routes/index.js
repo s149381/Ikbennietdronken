58eecf184d818b02fe6cbcfe380e11e9c00a3a5b
s149381 <jordi-klarenbeek@live.nl>
2016-01-13 15:29:55 +0100

    Update and install node-modules
    installing the changes in package.json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'AA Website' });
 });
 
+router.get('/login', function(req,res,next) {
+  res.render('login', { title: 'Login user'})
+});
+
 module.exports = router;
